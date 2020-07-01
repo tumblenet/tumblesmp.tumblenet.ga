e3c963a7df6ed7aff32c4648f701dda0ac0e53a8
Cameron Trow <tumblegamer@gmail.com>
2020-07-01 01:13:14 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	"use strict";
 	$("#countdown").countdown({
-		date: "20 july 2019 12:00:00", /** Enter new date here **/
+		date: "1 July 2020 12:00:00", /** Enter new date here **/
 		format: "on"
 	},
 	function() {
